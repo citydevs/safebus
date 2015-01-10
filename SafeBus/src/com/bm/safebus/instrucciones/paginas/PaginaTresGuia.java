428e8b20a8ea843c85b759da03cb9e22a14f60bb
mikesaurio <mikesaurio@gmail.com>
2015-01-10 14:51:34 -0600

    se agrega link para llamar a inmujeres en instructivo

--- SafeBus/src/com/bm/safebus/instrucciones/paginas/PaginaTresGuia.java
+++ SafeBus/src/com/bm/safebus/instrucciones/paginas/PaginaTresGuia.java
@@ -3,16 +3,14 @@ package com.bm.safebus.instrucciones.paginas;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bm.safebus.R;
-import com.bm.safebus.instrucciones.PaginadorInstrucciones;
-import com.bm.safebus.instrucciones.PaginadorInstrucciones.OnListenerCambiarTexto;
-import com.bm.savebus.utilerias.Utils;
 
 /**
  * pagina que muestra en una lista los adeudos de un carro con las secretarias
@@ -21,12 +19,11 @@ import com.bm.savebus.utilerias.Utils;
  *
  */
 @SuppressLint("ViewConstructor")
-public class PaginaTresGuia extends View implements OnListenerCambiarTexto {
+public class PaginaTresGuia extends View  {
 
 	
 	private View view;
 	private Activity context;
-	private static OnListenerMas onListenerMas;
 	private TextView p2_tv_titulo,p2_tv_da_click;
 	
 	
@@ -56,35 +53,18 @@ public class PaginaTresGuia extends View implements OnListenerCambiarTexto {
 	public void init() {
 		
 		LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		view = inflater.inflate(R.layout.instrucciones_pag_dos, null);
+		view = inflater.inflate(R.layout.instrucciones_pag_tres, null);
 		
-		
-		PaginadorInstrucciones.setOnClickCambiarTextoListener(this); //escucha del cambio de texto
-		
-		p2_tv_titulo =(TextView)view.findViewById(R.id.instrucciones_p2_tv_titulo);
-		p2_tv_da_click =(TextView)view.findViewById(R.id.instrucciones_p2_tv_da_click);
-		
-		String[] info= new Utils(context).getPreferenciasContacto();
-		if(info[0]!=null){
-			cambiarTexto(PaginadorInstrucciones.CONTACTO_NO_GUARDADO);
-				
-		} else {
-			cambiarTexto(PaginadorInstrucciones.CONTACTO_GUARDADO);
-		}
-		
-		
-		
-		
-		ImageView instrucciones_iv_mas= (ImageView)view.findViewById(R.id.instrucciones_iv_mas);
-		instrucciones_iv_mas.setOnClickListener(new View.OnClickListener() {
+		final TextView instrucciones_pag_3_tv_tel_inmujeres = (TextView)view.findViewById(R.id.instrucciones_pag_3_tv_tel_inmujeres);
+		instrucciones_pag_3_tv_tel_inmujeres.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
-				onListenerMas.onListenerMas();
-				
+				Intent callIntent = new Intent(Intent.ACTION_CALL);
+				callIntent.setData(Uri.parse("tel:+"+instrucciones_pag_3_tv_tel_inmujeres.getText().toString().trim()));
+				context.startActivity(callIntent );
 			}
 		});
-
 	}
 
 
@@ -97,45 +77,5 @@ public class PaginaTresGuia extends View implements OnListenerCambiarTexto {
 	}
 	
 	
-	/**
-	 * Interface que comunica la pagina con la actividad 
-	 * @author mikesaurio
-	 *
-	 */
-	 public interface OnListenerMas
-	    {
-	        void onListenerMas();
-	    }
-
-	 /**
-	  * escucha para poder dar click a siguiente pagina
-	  * @param listener
-	  */
-	public static void setOnClickSiguienteListener( OnListenerMas listener)
-	{
-		onListenerMas = listener;
-	}
-
-	@Override
-	public void onListenerCambiarTexto(int tipo) {
-		
-		cambiarTexto( tipo);
-	}
 	
-	
-	public void cambiarTexto(int tipo){
-		switch (tipo) {
-		case PaginadorInstrucciones.CONTACTO_GUARDADO:
-			p2_tv_titulo.setText(getResources().getString(R.string.paginas_instrucciones_texto_pag_2_1));
-			p2_tv_da_click.setText(getResources().getString(R.string.paginas_instrucciones_texto_pag_2_2));
-			break;
-		case PaginadorInstrucciones.CONTACTO_NO_GUARDADO:
-			p2_tv_titulo.setText(getResources().getString(R.string.paginas_instrucciones_texto_pag_2_1_listo));
-			p2_tv_da_click.setText(getResources().getString(R.string.paginas_instrucciones_texto_pag_2_2_listo));
-			break;
-
-		default:
-			break;
-		}
-	}
 }
